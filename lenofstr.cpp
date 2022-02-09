b37af5a5a8a8bf95c012ff8cc254e11acbd88951
Karan Gorai <92878327+krn563@users.noreply.github.com>
2022-02-09 22:48:50 +0530

    Add files via upload

---
+++ lenofstr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int getlength(char name[]){
+    int count=0;
+    for(int i=0; name[i]!='\0';i++){
+        count++;
+    }
+    return count;
+}
+int main(){
+    char name[20];
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<"Your name is "<<name<<endl;
+    cout<<"The length of string is:"<<getlength(name);
+    return 0;
+}
