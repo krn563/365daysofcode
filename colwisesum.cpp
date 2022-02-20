ca5f050513aa76eeb5defd533e17136515655e0f
Karan Gorai <92878327+krn563@users.noreply.github.com>
2022-02-20 12:06:31 +0530

    Add files via upload

---
+++ colwisesum.cpp
@@ -0,0 +1,30 @@
+#include<iostream> 
+using namespace std;
+void printcolsum(int arr[][3],int i,int j){
+    for(int j=0;j<3;j++){
+        int sum=0;
+        for(int i=0;i<3;i++){
+            sum+=arr[i][j];
+        }
+        cout<<sum<<" ";
+    }
+    cout<<endl;
+}
+int main() {
+    cout<<"start";
+    int arr[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    printcolsum(arr,3,3);
+    return 0;
+}
