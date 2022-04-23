081bf830f6dcfb57a6a18ad5f6de7cdebcbb853c
Karan Gorai <92878327+krn563@users.noreply.github.com>
2022-04-23 21:54:45 +0530

    Add files via upload

---
+++ stringstream.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+vector<int> parseInts(string str) {
+    stringstream ss(str);
+    vector<int> result;
+    char ch;
+    int tmp;
+    while(ss >> tmp) {
+        result.push_back(tmp);
+        ss >> ch;
+    }
+    return result;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++){
+        cout << integers[i] << "\n";
+    }  
+    return 0;
+}
